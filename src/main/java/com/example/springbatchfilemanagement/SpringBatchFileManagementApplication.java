c37ddeaeae1ed4f72a815b9c09666385323d47e5
Assia FADLI <assia.fadli@fabrick.io>
2023-03-21 14:27:10 +0100

    init project

---
+++ src/main/java/com/example/springbatchfilemanagement/SpringBatchFileManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchfilemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchFileManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchFileManagementApplication.class, args);
+    }
+
+}
